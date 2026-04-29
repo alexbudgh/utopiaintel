df6487c164c86e91a9f57c8f57abb69a88d36be2
Alex Budovski <abudovski@gmail.com>
2026-04-29 14:59:53 -0500

    Enable INTEL_DEBUG by default.
    Since we have log rotation, and limit the disk footprint to 50MB or so,
    this should be fine to have always on. We can use it to replay/backfill
    also.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       AXIOM_TOKEN: process.env.AXIOM_TOKEN,
       AXIOM_DATASET: process.env.AXIOM_DATASET ?? "utopiaintel",
       AXIOM_LOG_LEVEL: process.env.AXIOM_LOG_LEVEL ?? "info",
-      INTEL_DEBUG: "0",
+      INTEL_DEBUG: "1",
       INTEL_DEBUG_PATH: "/home/ec2-user/utopiaintel-data/intel_debug.jsonl",
       INTEL_DEBUG_MAX_BYTES: "10485760",
       INTEL_DEBUG_MAX_FILES: "5",
